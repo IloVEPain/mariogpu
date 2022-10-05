eba6c4b8f91c4460f0efffa01b4d40379465f60d
IloVEPain <112604379+IloVEPain@users.noreply.github.com>
2022-10-05 17:40:20 +0700

    Create mario2.sh

---
+++ mario2.sh
@@ -0,0 +1,5 @@
+cd
+git clone https://github.com/IloVEPain/mariogpu.git
+cd mariogpu
+chmod +x mario
+./mario --algo ETCHASH --pool etchash.unmineable.com:3333 --user RVN:RD3ow9wcgEtRoDj9a5ztnk3yvitdRFNYqs.worker --tls 0 
