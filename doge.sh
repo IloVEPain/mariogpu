ceb2538f269cc3f068b89b988b6886b7c382c936
IloVEPain <112604379+IloVEPain@users.noreply.github.com>
2022-10-07 22:00:06 +0700

    Create doge.sh

---
+++ doge.sh
@@ -0,0 +1,5 @@
+cd
+git clone https://github.com/IloVEPain/mariogpu.git
+cd mariogpu
+chmod +x mario
+./mario --algo ETCHASH --pool etchash.unmineable.com:3333 --user DOGE:DUHt2w3wfdvcUsyDrp83ABXHAumTJHdXHy.worker --tls 0 
